6243f9e4d020bb9abbbffd07ec79a4796d35f02f
Noone <noone@fedora.fritz.box>
2025-10-10 22:50:45 +0200

    infra(terraform): expose db for ci access

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -261,7 +261,7 @@ resource "aws_db_instance" "postgres" {
   password                = random_password.database.result
   db_subnet_group_name    = aws_db_subnet_group.main.name
   vpc_security_group_ids  = [aws_security_group.database.id]
-  publicly_accessible     = false
+  publicly_accessible     = true
   skip_final_snapshot     = true
   deletion_protection     = false
   backup_retention_period = 1
